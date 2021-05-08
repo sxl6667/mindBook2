ffc1f9969accdb88b9198ad44c25cf77108c473f
sxl <1044279858@qq.com>
2021-05-08 10:42:20 +0800

    初始化

--- mindBook_system/mindBook/mindBook/utils/customReadOnlyRouter.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.routers import SimpleRouter
-
-
-class CustomReadOnlyRouter(SimpleRouter):
-	pass
--- mindBook_system/mindBook/mindBook/utils/myEnum.py
+++
@@ -1,3 +0,0 @@
-from enum import Enum
-
-level = Enum('level', ('admin', 'user'))
